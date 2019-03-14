14dd8ca98f208c899b4a2cf6844e8e3be78433c6
martinlistwan <martinc.listwan@hotmail.com>
2019-03-14 06:07:13 -0400

    almost done 2 and working on third

---
+++ app/src/main/java/capstone/project/curl/QuickAnswerSmsParser.java
@@ -0,0 +1,8 @@
+package capstone.project.curl;
+
+/**
+ * Created by Marti on 2019-03-14.
+ */
+
+public class QuickAnswerSmsParser {
+}
---
+++ app/src/main/java/capstone/project/curl/StateAdapter/QuickAnswerAdapter.java
@@ -0,0 +1,8 @@
+package capstone.project.curl.StateAdapter;
+
+/**
+ * Created by Marti on 2019-03-14.
+ */
+
+public class QuickAnswerAdapter {
+}
